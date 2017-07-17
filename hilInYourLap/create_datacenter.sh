a9c7702bab81935eda52bd154854a7b35380e095
SahilTikale <sahil.tikale@gmail.com>
2017-07-17 16:17:54 -0400

    Adds a script to creates a dummy datacenter for HIL. with this script, arbitary amount of nodes that connect via openvswitch are created and cabled. HIL can register this nodes and its nics, the switch and its ports and operate on them like real nodes and switches.

---
+++ hilInYourLap/create_datacenter.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/bash
+# Following script takes as a digit between 1 to 99
+# creates as many network namespaces.
+
+create_switch() {
+  switch_name=$1
+  ovs-vsctl add-br $switch_name
+  ip link set dev $switch_name up
+  ovs-vsctl show
+
+}
+
+create_nodes() {
+  no_of_nodes=$1 # any number between 1 to 99
+  switch_name=$2 # name of the created in openvswitch.
+  echo "creating nodes from node-0 to node-$no_of_nodes"
+  sleep 2
+  for ((i=0; i<=no_of_nodes; i++));
+    do
+      ip netns add node-$i; #create Nodes (empty network namespaces)
+      ip link add eth0-$i type veth peer name veth-$i # Creating a virtual cables 
+    	# with eth0-xx end connected to the node and 
+	#veth-xx left as it is to be connected to the switch at later point.
+
+      ip link set eth0-$i netns node-$i 
+      # Connect eth0-$i end of cable to nodes.
+   done
+   ovs-vsctl show
+   ip netns list
+}
+
+connect_node2switch () {
+  no_of_nodes=$1
+  switch_name=$2
+  echo "Connecting nodes from node-<0-$no_of_nodes> to $switch_name." 
+  sleep 2
+  for ((i=0; i<=no_of_nodes; i++));
+    do
+      ovs-vsctl add-port $switch_name veth-$i
+    done
+    ovs-vsctl show
+}
+
+fullsetup () {
+  no_of_nodes=$1
+  switch_name=$2
+  echo "Creating a datacenter having $1 nodes and a switch called $switch_name"
+  sleep 2
+  create_switch $switch_name
+  create_nodes $no_of_nodes $switch_name
+  connect_node2switch $no_of_nodes $switch_name
+}
+
+test_loop() {
+	total=$1
+	for ((i=01; i<=total; i++));
+	do
+	echo $i
+	done
+}
+
+
+case "$1" in 
+  -switch)
+    create_switch $2
+    ;;
+  -nodes)
+    create_nodes $2 $3 
+    ;;
+  -connect_node2switch)
+     connect_node2switch $2 $3
+     ;;
+  -fullsetup)
+    fullsetup $2 $3
+    ;;
+  loop)
+    test_loop $2 
+    ;;
+    *) 
+    echo $" sudo privileges required for this script. " 
+    echo $"Usage: $0 "
+    echo $"  	-switch <switch_name> "
+    echo $"		# Creates an openvswitch by that name"
+    echo $"  	-nodes <no_of_nodes> <switch_name> "
+    echo $"			# Creates nodes (netns) "
+    echo $"  	-cable nodes <no_of_nodes> <switch_name> "
+    echo $"			# Connects nodes to <switch_name>"
+    echo $"   Running them in this order. "
+    echo $" "
+    echo $"$0 -fullsetup <no_of_nodes> <switch_name>"
+    echo $"		# will setup a mock infrastructure with a mock switch"
+    echo $"  and netns as nodes connect via veth pair of cables. "
+    echo $"  By default the nodes are offline. They need to be activated when needed. "
+    echo ""
+    exit 1
+    ;;
+esac
+
